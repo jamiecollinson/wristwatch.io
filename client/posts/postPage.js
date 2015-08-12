79a0e2078f940d188c56b112ff156691153dee3c
Jamie Collinson <jamiecollinson@gmail.com>
2015-08-12 15:13:07 +0100

    Add not found route and redirect from postpage if no post data found

--- client/posts/postPage.js
+++ client/posts/postPage.js
@@ -1,5 +1,15 @@
 Template.postPage.onCreated(function() {
   this.subscribe('posts');
+  // redirect if no post found
+  this.autorun(function(){
+    if (Template.instance().subscriptionsReady()) {
+      var id = FlowRouter.getParam('id'),
+          post = Posts.findOne({ _id: id });
+      if (!post) {
+        FlowRouter.go('/');
+      }
+    }
+  });
 });
 
 Template.postPage.helpers({
